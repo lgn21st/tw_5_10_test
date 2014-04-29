2e0520c333342768cf35021dbfdf570dde10f7ae
Daniel Lv <lgn21st@gmail.com>
2014-04-29 14:04:47 +0800

    empty FizzBuzzWhizz class

---
+++ lib/fizz_buzz_whizz.rb
@@ -0,0 +1,3 @@
+class FizzBuzzWhizz
+
+end
--- test/test_fizz_buzz_whizz.rb
+++ test/test_fizz_buzz_whizz.rb
@@ -3,3 +3,8 @@ $:.unshift File.expand_path(File.join(File.dirname(__FILE__), 'lib'))
 require "minitest/autorun"
 require 'fizz_buzz_whizz'
 
+describe FizzBuzzWhizz do
+  let(:fbw) { FizzBuzzWhizz.new }
+
+  it { fbw.must_be_instance_of FizzBuzzWhizz }
+end
