d82d5cf2120421858a09e4f4a3ce65cad7ebd7bf
Daniel Lv <lgn21st@gmail.com>
2014-04-29 14:13:17 +0800

    init with default factor number

--- lib/fizz_buzz_whizz.rb
+++ lib/fizz_buzz_whizz.rb
@@ -1,3 +1,10 @@
 class FizzBuzzWhizz
+  attr_accessor :fizz, :buzz, :whizz
+
+  def initialize(fizz=3, buzz=5, whizz=7)
+    @fizz = fizz
+    @buzz = buzz
+    @whizz = whizz
+  end
 
 end
--- test/test_fizz_buzz_whizz.rb
+++ test/test_fizz_buzz_whizz.rb
@@ -7,4 +7,13 @@ describe FizzBuzzWhizz do
   let(:fbw) { FizzBuzzWhizz.new }
 
   it { fbw.must_be_instance_of FizzBuzzWhizz }
+
+  describe "init with default factor number" do
+    let(:fbw) { FizzBuzzWhizz.new }
+
+    it { fbw.fizz.must_equal 3 }
+    it { fbw.buzz.must_equal 5 }
+    it { fbw.whizz.must_equal 7 }
+  end
+
 end
